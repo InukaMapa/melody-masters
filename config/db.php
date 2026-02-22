45ea2b0c584055b1fb1d48c1ee61b7c9e8bd3a96
Inuka Mapa <139098767+InukaMapa@users.noreply.github.com>
2026-02-22 20:45:05 +0530

    database connection

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+$dbname = "melody_masters";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Set charset
+$conn->set_charset("utf8mb4");
+
+?>
